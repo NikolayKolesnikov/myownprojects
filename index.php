39d3ec3eee583f126dd123414f38c1eac5bdb8b9
Nikolay Kolesnikov <dnepro_man@mail.ru>
2016-01-22 20:11:13 +0200

    to php, include fx, etc

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Lineage Mini</title>
+    <meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    <script src="main.js"></script>
+</head>
+ 
+<body>
+    
+    
+    
+    
+    
+</body>
+</html>
